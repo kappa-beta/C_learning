33f721428a0e25b0f9ffe51eb14dcaeced3b425c
GKV <gkv@tz74.ru>
2022-08-31 11:28:02 +0500

    add task 1.14

--- vI/1.06/1.14.c
+++ vI/1.06/1.14.c
@@ -5,30 +5,31 @@
 
 #define DIMENSION 128
 
-int main (void) {
-	int i, j, c, k;
-	int ch[DIMENSION];
-	
-	for (i = 0; i < DIMENSION; ++i) {
-		ch[i] = 0;
-	}
-	
-	while ((c = getchar()) != EOF) {	
-		++ch[c];
-	}
-	
-	for (i = 0; i < DIMENSION; ++i) {
-		k = ch[i];
-		if (k != 0) {
-			putchar(i);
-			putchar(' ');
-			for (j = 0; j < k; ++j) {
-				putchar('_');
-			}
-			printf(" %d", k);
-			putchar('\n');
-		}
-	}
-	
-	return 0;
+int main(void) {
+    int i, j, c;
+    int array[DIMENSION]; // массив символов
+
+    // заполнение массива нулями
+    for(i = 0; i < DIMENSION; ++i)
+        array[i] = 0;
+
+    while((c = getchar()) != EOF) {
+        if(c == '\n' || c == ' ' || c == '\t')
+            array[c] = 0;
+        else
+            ++array[c];
+    }
+
+    // вывод результата
+    for(i = 0; i < DIMENSION; ++i) {
+        if(array[i] != 0) {
+            printf("%3d %c ", i, i);
+            for(j = 0; j < array[i]; ++j) {
+                printf("_");
+            }
+            printf("\n");
+        }
+    }
+
+    return 0;
 }
